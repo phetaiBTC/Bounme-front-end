de4e6fc8b1046deb8e31cc850f6331336424ab69
unknown <phetaibtc@gmail.com>
2025-03-20 16:38:05 +0700

    setup

---
+++ components/changeLanguage.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+// import { useI18n } from 'vue-i18n'
+
+// const { locale } = useI18n()
+
+// const changeLanguage = (lang: string) => {
+//     locale.value = lang
+//     localStorage.setItem('lang', lang)  
+// }
+</script>
+
+<template>
+    <a-button type="primary">EN</a-button>
+</template>
---
+++ plugins/i18n.ts
@@ -0,0 +1,17 @@
+import { createI18n } from 'vue-i18n'
+import en from '~/locales/en.json'
+import la from '~/locales/la.json'
+export default defineNuxtPlugin((nuxtApp) => {
+    const savedLocale = localStorage.getItem('lang') ? 'en' : 'la'
+    const i18n = createI18n({
+        legacy: false, // Use Composition API
+        locale: savedLocale,
+        fallbackLocale: 'en',
+        messages: {
+            en,
+            la
+        }
+    })
+
+    nuxtApp.vueApp.use(i18n)
+})
