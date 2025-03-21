5b187811b44fefb07d07781fada9db7133249296
unknown <phetaibtc@gmail.com>
2025-03-21 09:18:06 +0700

    update layout

--- pages/phet.vue
+++ pages/phet.vue
@@ -5,7 +5,7 @@ const { locale } = useI18n()
 
 const changeLanguage = (lang: string) => {
     locale.value = lang
-    localStorage.setItem('lang', lang)
+    // localStorage.setItem('lang', lang)
 }
 </script>
 
@@ -14,5 +14,8 @@ const changeLanguage = (lang: string) => {
         <a-button :type="locale === 'en' ? 'primary' : 'default'" @click="changeLanguage('en')">English</a-button>
         <a-button :type="locale === 'la' ? 'primary' : 'default'" @click="changeLanguage('la')">Laos</a-button>
     </div>
-    {{ $t('hello') }}
+    <h1 class="text-2xl">
+        {{ $t('hello') }}
+    </h1>
+
 </template>
